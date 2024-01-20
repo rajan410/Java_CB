f67ac6126d84962aac196ef568e70f5caaaf5d66
rajan410 <157243840+rajan410@users.noreply.github.com>
2024-01-21 01:17:10 +0530

    lecture 2

---
+++ Lec_2/Print_hello_world.java
@@ -0,0 +1,10 @@
+package Lec_2;
+
+public class Print_hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
---
+++ Lec_2/lec_3.java
@@ -0,0 +1,15 @@
+package Lec_2;
+import java.util.*;
+public class lec_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner hp=new Scanner(System.in);
+		int a=hp.nextInt();
+//		int b=hp.nextInt();
+//		System.out.println(a%b);
+		a +=4;
+		System.out.println(a);
+	}
+
+}
